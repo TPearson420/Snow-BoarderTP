532ff917cb4d420e582664f402e4585d1213411e
TPearson420 <pearso420@morrisville.edu>
2023-10-01 01:57:04 -0400

    Using Invoke for Delays

--- Snow-BoarderTP/Assets/Scripts/CrashDetector.cs
+++ Snow-BoarderTP/Assets/Scripts/CrashDetector.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.5f;
      void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground") 
-        { SceneManager.LoadScene(0); }
+        if (other.tag == "Ground")
+        { Invoke("ReloadScene", loadDelay); }
+    }
+        void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+        
     }
 }
